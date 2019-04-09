78b91b37d8e69a561bd57a9023622bb2d73ed51e
RoRx070yue <49121424+RoRx070yue@users.noreply.github.com>
2019-04-09 16:26:15 +0800

    Update distanceCalculation.java

--- distanceCalculation.java
+++ distanceCalculation.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class distanceCalculation {
-¡@¡@public static void main(String args[]){
+    public static void main(String args[]){
 
 	Scanner scan = new Scanner(System.in);
 	int x1,x2,y1,y2;
@@ -17,5 +17,5 @@ public class distanceCalculation {
 	double dist = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
 
 	System.out.printf("the distance between (%.2f,%.2f) and (%.2f,%.2f) is:%.2f",(double)x1,(double)y1,(double)x2,(double)y2,dist);
-¡@¡@}
+    }
 }
